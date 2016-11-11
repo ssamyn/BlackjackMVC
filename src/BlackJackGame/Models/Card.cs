5d40a098b71cc428c4fcb6c720e7f260652b06d4
Stefaan Samyn <stefaan.samyn@hogent.be>
2016-11-11 14:58:33 +0100

    Add Models and unit tests

---
+++ src/BlackJackGame/Models/Card.cs
@@ -0,0 +1,17 @@
+﻿namespace BlackJackGame.Models {
+    public class Card {
+
+        #region Properties
+        public FaceValue FaceValue { get; private set; }
+        public Suit Suit { get; private set; }
+        #endregion
+
+        #region Constructors
+        public Card(Suit suit, FaceValue faceValue) {
+            Suit = suit;
+            FaceValue = faceValue;
+        }
+        #endregion
+
+        }       
+    }
