1b2b8860b28cfdbfd25a748dead0dd55b702b63d
Stefaan Samyn <stefaan.samyn@hogent.be>
2016-11-11 15:19:43 +0100

    Add functionality for Index

---
+++ src/BlackJackGame/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlackJackGame.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BlackJackGame.Controllers
+{
+    public class HomeController : Controller
+    {
+        private BlackJack _blackjack;
+        public IActionResult Index()
+        {
+            _blackjack = new BlackJack();
+            return View(_blackjack);
+        }
+    }
+}
--- src/BlackJackGame/Startup.cs
+++ src/BlackJackGame/Startup.cs
@@ -16,6 +16,7 @@ namespace BlackJackGame
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,6 +28,12 @@ namespace BlackJackGame
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseStaticFiles();
+            app.UseMvc(routes => {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
 
             app.Run(async (context) =>
             {
